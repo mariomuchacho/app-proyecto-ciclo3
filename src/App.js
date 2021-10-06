08e8866cb366d3be0a30c1cbb2a76f8e8c935d6d
“yonier” <yonier.andresng@gmail.com>
2021-10-05 23:43:36 -0500

    prueba con app para manejo de componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { TodoCounter } from './components/TodoCounter'
+import { TodoSearch} from './components/TodoSearch'
+import { TodoList} from './components/TodoList'
+import { TodoItem } from './components/TodoItem'
+import { CreateTodoButton } from './components/CreateTodoButton'
+const todos = [
+  { text: 'cortar cebolla', completed: false},
+  { text: 'terminar el curso de react', completed: false},
+  { text: 'comer la cebolla', completed: false}
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+   <React.Fragment>
+
+    <TodoCounter />
+
+    <TodoSearch />
+   
+    <TodoList>
+
+     {todos.map(todo => (
+      <TodoItem key={todo.text} text={todo.text} />
+     ))}
+
+    </TodoList>
+
+    <CreateTodoButton />
+   
+   </React.Fragment>
+  );
+};
 export default App;
