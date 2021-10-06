b8eae22485d2937716e440ddf4cdb90c54d00929
Mario <apple@MacBook-Pro-de-Apple.local>
2021-10-06 18:14:53 -0500

    correccion de archivos rafce

---
+++ src/pages/GestionVendedores.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GestionVendedores = () => {
+  return <div></div>;
+};
+
+export default GestionVendedores;
---
+++ src/pages/RegistrarVenta.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RegistrarVenta = () => {
+  return <div></div>;
+};
+
+export default RegistrarVenta;
