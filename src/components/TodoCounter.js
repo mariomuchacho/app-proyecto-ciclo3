069675120c131d633e8efa60c9d5d1bca4850bf1
“yonier” <yonier.andresng@gmail.com>
2021-10-08 19:20:27 -0500

    probando componentes y estructura con css

--- src/components/TodoCounter.js
+++ src/components/TodoCounter.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../css/TodoCounter.css';
 
 function TodoCounter() {
-    return(
-        <h2>has completado 2 de 3 TODOs</h2>
+    return (
+        <h2 className="TodoCounter"> has completado 2 de 3 TODOs</h2>
     );
 }
 
--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../css/TodoSearch.css';
 
 function TodoSearch() {
     return(
-        <input placeholder="cebolla" />
+        <input className="TodoSearch" placeholder="cebolla" />
     );
 }
 
